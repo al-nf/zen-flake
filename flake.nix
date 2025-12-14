11e24b22dc675557aa2825ec41e24983a47156ea
Alan Fung <alankjfung@gmail.com>
2025-12-14 15:10:37 -0800

    erm

--- flake.nix
+++ flake.nix
@@ -1,51 +1,28 @@
-{
-  description = "Zen Browser (AppImage) flake";
+packages.${system}.zen-browser =
+  pkgs.stdenv.mkDerivation {
+    pname = "zen-browser";
+    version = "1.17.13b";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-
-      version = "1.17.13b";
-    in
-    {
-      packages.${system}.zen-browser =
-        let
-          wrapped = pkgs.appimageTools.wrapType2 {
-            pname = "zen-browser";
-            inherit version;
-
-            src = pkgs.fetchurl {
-              url =
-                "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
-              sha256 = "sha256-hBDQW8a1KsFDDT+2QYR27BZclbMXh3Zb92QnmqmuIPA=";
-            };
-
-            buildInputs = [ pkgs.xdg_utils ];
-
-            extraInstallCommands = ''
-              mkdir -p $out/share/applications
-              cat > $out/share/applications/zen-browser.desktop <<EOF
-              [Desktop Entry]
-              Name=Zen
-              Exec=zen-browser
-              Icon=zen-browser
-              Type=Application
-              Categories=Network;WebBrowser;
-              EOF
-            '';
-          };
-        in
-        pkgs.makeWrapper wrapped {
-          name = "zen-browser";
-          setEnv = {
-            BROWSER = "$out/bin/zen-browser";
-          };
-        };
-
-      defaultPackage.${system} =
-        self.packages.${system}.zen-browser;
+    src = pkgs.fetchurl {
+      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
+      sha256 = "sha256-hBDQW8a1KsFDDT+2QYR27BZclbMXh3Zb92QnmqmuIPA=";
     };
-}
+
+    buildInputs = [ pkgs.appimageTools pkgs.xdg_utils pkgs.makeWrapper ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/zen-browser
+      wrapProgram $out/bin/zen-browser --set BROWSER $out/bin/zen-browser
+
+      mkdir -p $out/share/applications
+      cat > $out/share/applications/zen-browser.desktop <<EOF
+      [Desktop Entry]
+      Name=Zen
+      Exec=zen-browser
+      Icon=zen-browser
+      Type=Application
+      Categories=Network;WebBrowser;
+      EOF
+    '';
+  };
