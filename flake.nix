fa1b4be8739ab1776a9af226a9bb7e9f919afaa2
Alan Fung <alankjfung@gmail.com>
2025-12-13 17:49:39 -0800

    updated flake

--- flake.nix
+++ flake.nix
@@ -12,31 +12,40 @@
     in
     {
       packages.${system}.zen-browser =
-        pkgs.appimageTools.wrapType2 {
-          pname = "zen-browser";
-          inherit version;
+        let
+          wrapped = pkgs.appimageTools.wrapType2 {
+            pname = "zen-browser";
+            inherit version;
 
-          src = pkgs.fetchurl {
-            url =
-              "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
-            sha256 = "sha256-hBDQW8a1KsFDDT+2QYR27BZclbMXh3Zb92QnmqmuIPA=";
+            src = pkgs.fetchurl {
+              url =
+                "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
+              sha256 = "sha256-hBDQW8a1KsFDDT+2QYR27BZclbMXh3Zb92QnmqmuIPA=";
+            };
+
+            buildInputs = [ pkgs.xdg_utils ];
+
+            extraInstallCommands = ''
+              mkdir -p $out/share/applications
+              cat > $out/share/applications/zen-browser.desktop <<EOF
+              [Desktop Entry]
+              Name=Zen
+              Exec=zen-browser
+              Icon=zen-browser
+              Type=Application
+              Categories=Network;WebBrowser;
+              EOF
+            '';
           };
-
-          extraInstallCommands = ''
-            mkdir -p $out/share/applications
-            cat > $out/share/applications/zen-browser.desktop <<EOF
-            [Desktop Entry]
-            Name=Zen
-            Exec=zen-browser
-            Icon=zen-browser
-            Type=Application
-            Categories=Network;WebBrowser;
-            EOF
-          '';
+        in
+        pkgs.makeWrapper wrapped {
+          name = "zen-browser";
+          setEnv = {
+            BROWSER = "$out/bin/zen-browser";
+          };
         };
 
       defaultPackage.${system} =
         self.packages.${system}.zen-browser;
     };
 }
-
